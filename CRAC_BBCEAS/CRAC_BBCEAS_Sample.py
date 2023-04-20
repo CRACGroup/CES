65c1a018fb3f59a004625ffb27f1a9ba6da6ee9e
mixtli-c <mixtli.campos@atmosfera.unam.mx>
2023-04-20 16:20:32 +0100

    working on plotting

--- CRAC_BBCEAS/CRAC_BBCEAS_Sample.py
+++ CRAC_BBCEAS/CRAC_BBCEAS_Sample.py
@@ -35,7 +35,7 @@ from pyAndorSDK2 import atmcd_errors as errors
 #####                       PARAMETER CONFIGURATION                                 #####
 # These parameters can be changed manually or with a configuration file
 
-### Instrument
+### Instrument 
 temp = conf.temp                                    # Camera temperature
 exptime = conf.exptime                              # Exposure time in seconds
 bckg_shots = conf.bckg_shots                        # Number of background shots
@@ -50,8 +50,34 @@ accum_cycle = exptime + conf.delay                  # Exp + Delay = Cycle time
 readMode = conf.readMode                            # Read mode
 trigMode = conf.trigMode                            # Trigger Mode
 
+### Signal analysis
+# Cavity parameters
+distance = conf.distance                    # Sample optical length
+
+# Resonance window 
+lower_wavelength=conf.lower_wavelength      # Starting wavelength of resonance window
+upper_wavelength=conf.upper_wavelength      # Ending wavelength of resonance window
+
+# Reference and background files
+back_filename = conf.back_filename
+no2_refname = conf.no2_refname
+chocho_refname = conf.chocho_refname
+
+# Reff : Either a number conf.Reff or a vector np.load(conf.Reff_matrix)
+Reff= conf.Reff
+
+# Dilution factor --> SET TO 1 for IASC
+dfactor = 1
+#dfactor = 1-(conf.n2flow/conf.tflow)
+
 ### Path for saving data
 savepath = conf.savepath
+
+#########################################################################################
+### Reference and Background file loading for analisis                                ###
+no2reference = np.load(no2_refname)
+chochoref = np.load(chocho_refname)
+background = np.load(back_filename)
 
 #########################################################################################
 ##### Making a subdirectory for generated files %Y%m%d
@@ -114,16 +140,31 @@ wavelengths = cf.andor_calibrator(xpixels,*conf.calfactors)
 
 ### Initialize plot
 fig = plt.figure()              # Figure initialization
-ax1 = fig.add_subplot(111)      # Axes 1 : Signal
+ax1 = fig.add_subplot(211)      # Axes 1 : Signal
+ax2 = fig.add_subplot(212)      # Axes 2 : Concentration 1
+ax3 = ax2.twinx()               # Axes 3 : Concentration 2
+
+# Initialize empty plots
+minwave,maxwave = cf.segment_indices(background[:,0:2],lower_wavelength,
+            upper_wavelength)
 ax1.set_ylim([0,500])
+ax2.set_ylim([0,500])
+ax3.set_ylim([0,500])
 xs = list(range(0,xpixels))
 ys = [0] * xpixels
 line, = ax1.plot(xs,ys,'-k')
+line1, = ax1.plot(xs,ys,'-g')
+line2, = ax2.plot(xs,ys,'-b')
+line3, = ax3.plot(xs,ys,'-r')
 
 #t0 = dt.datetime.now() # testing for total elapsed time
 
-### Initialize measurement array
-measurements = np.copy(wavelengths).reshape(len(wavelengths),1)
+### Initializing timestamp and concentration list, and measurement array
+measurements = np.array(background[:,0]).reshape(len(background[:,0]),1)
+meastime = np.zeros(samples,dtype='U19')
+meastime2 = []
+ppbs = np.zeros(samples)
+ppbs2 = np.zeros(samples)
 
 # Perform Acquisition loop as an animate function
 
@@ -148,15 +189,47 @@ def animate(i):
     #    ret, arr[0], xpixels))
     #print(arr.shape)
 
+    ### Calculating number density
+    counts = np.copy(arr).reshape(len(arr),1)
+    minwave,maxwave = cf.segment_indices(measurements[:,0:2],lower_wavelength,
+            upper_wavelength)
+    bckg = np.copy(background[minwave:maxwave,:])
+    no2ref = np.copy(no2reference[minwave:maxwave,:])
+    glyref = np.copy(chochoref[minwave:maxwave,:])
+    I_sample = np.copy(sample[minwave:maxwave,:])
+    I_0 = np.average(bckg[:,1:],axis=1).reshape(len(bckg),1)
+    
+    ### This one does everything (see recursive_fit_2ref function in CESfunctions.py)
+    alpha,fl,a,b,ndensity1, ndensity2 = cf.fit_alg_1(I_sample, I_0, Reff, distance, 
+            no2ref,glyref,parameters=1)
+    
+    ### The timestamp for this measurement is now
+    timenow = dt.datetime.now()
+    stamp = timenow.strftime('%y%m%d%H%M%S')
+    meastime2.append(timenow)
+
+    ### Add sample to measurements array and save individual sample datafile
+    measurements = np.concatenate((measurements,counts.reshape(len(counts),1)),axis=1)
+    
+    np.savetxt(path_file+'Is'+stamp+'.txt',measurements[:,[0,n+1]],fmt='%s')
+
+    ### Populate ppbs and meastime arrays with currents sample, make/overwrite datafile
+    ppbs[n] = (ndensity1/2.504e10)/dfactor
+    ppbs2[n] = (ndensity2/2.504e10)/dfactor
+    meastime[n] = timenow.strftime('%Y/%m/%d-%H:%M:%S')
+    area[n] = np.trapz(counts[minwave:maxwave+1])
+       
+    np.savetxt(path_file+'Mtemp.txt',np.column_stack((meastime[:n+1],ppbs[:n+1],
+        ppbs2[:n+1],area[:n+1])), fmt='%s')
+
+    # Print calculated NO2 in ppb
+    print('NO2 ppb: ', ppbs[n], 'CHOCHO ppb: ', ppbs2[n])
+
     ### Plotting
     ax1.set_ylim([min(arr)-10,max(arr)+10])
     line.set_ydata(arr)
     
-    ### Making arrays
-    counts = np.copy(arr).reshape(len(arr),1)
-    measurements = np.concatenate((measurements,counts),axis=1)
-
-    return line,
+    return line, line1, line2, line3,
 
 # call animation
 ani = animation.FuncAnimation(fig,animate, frames=bckg_shots
